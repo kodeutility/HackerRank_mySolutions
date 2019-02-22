2b98fdd05572b363f657c2de4ecdfcacbb207355
Kiran <kodeutility@gmail.com>
2019-02-22 21:29:04 +0530

    say_hello_world_with_python solution is completed

---
+++ Introduction/say_hello_world_with_python.py
@@ -0,0 +1,6 @@
+# HackerRank        : https://www.hackerrank.com/kodeutility
+# GitHub            : https://www.github.com/kodeutility
+# Portfolio website : https://kodeutility.github.io/
+# Author            : Kiran BG
+
+print("Hello, World!")
