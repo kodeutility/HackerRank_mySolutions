c2d902a75bf7ca2c590957727c9376a145c965e7
Kiran <kodeutility@gmail.com>
2019-03-01 22:57:56 +0530

    arithmetic_operators solution is added

---
+++ Introduction/arithmetic_operators.py
@@ -0,0 +1,12 @@
+# HackerRank        : https://www.hackerrank.com/kodeutility
+# GitHub            : https://www.github.com/kodeutility
+# Portfolio website : https://kodeutility.github.io/
+# Author            : Kiran BG
+
+if __name__ == '__main__':
+    number_1 = int(input())
+    number_2 = int(input())
+
+    print(number_1+number_2)
+    print(number_1-number_2)
+    print(number_1*number_2)
